82ef348ce84b18382834132448774b296c937a40
pyotruk <pyotruk@gmail.com>
2016-09-14 00:11:02 +0300

    sensor async

--- mqtt.ino
+++ mqtt.ino
@@ -15,6 +15,11 @@ const char *mqtt_pass = "123123"; // Пароль от сервера
 const char *mqtt_topic = "test"; // Название топика
 
 const int DEVICE = LED_BUILTIN; // порт утсройства, которым мы управляем (светодиод или реле)
+const int SENSOR_PERIOD = 60; // секунд
+
+float calcDewPoint(float t, float h) {
+  return t - ((1 - h/100) / 0.05);
+}
 
 // Функция получения данных от сервера
 void callback(char* topic, uint8_t* payload, unsigned int length) {
@@ -36,8 +41,7 @@ void callback(char* topic, uint8_t* payload, unsigned int length) {
 WiFiClient wclient; 
 PubSubClient client(mqtt_server, mqtt_port, wclient);
 DHT dht(2, DHT22, 15);
-
-int cnt = 0;
+int count = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -50,7 +54,7 @@ void setup() {
 }
 
 void loop() {
-  
+
   // подключаемся к wi-fi
   if (WiFi.status() != WL_CONNECTED) {
     
@@ -83,21 +87,27 @@ void loop() {
       client.subscribe(mqtt_topic); // подписываемся на топик с данными для светодиода
         
     } else {
-      Serial.println("Could not connect to MQTT server"); 
+     Serial.println("Could not connect to MQTT server"); 
     }
   }
 
-  if (client.connected() && ((++cnt % 1000) == 0)) {
-    
+  if (client.connected() && ((++count % (10 * SENSOR_PERIOD)) == 0)) {
+
     float temperature = dht.readTemperature();
     float humidity = dht.readHumidity();
+    float dewPoint = calcDewPoint(temperature, humidity);
 
     String msg = "T=";
     msg += temperature;
-    msg += ", H=";
+    msg += "___H=";
     msg += humidity;
+    msg += "___Tp=";
+    msg += dewPoint;
 
-    if (client.publish(mqtt_topic, (char*)&msg)) {
+    char data[42];
+    msg.toCharArray(data, 42);
+
+    if (client.publish(mqtt_topic, data)) {
       Serial.println("OK >> published.");
       
     } else {
@@ -109,5 +119,6 @@ void loop() {
     client.loop();
   }
 
+  delay(100);
 }
 
